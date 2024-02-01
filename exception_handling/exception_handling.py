ab344e89417c9fe62f3395d68fca6480a2210a6b
Ganesh <gjkkunwar07@gmail.com>
2024-02-01 21:52:06 +0545

    updated finally of exception handling

--- exception_handling/exception_handling.py
+++ exception_handling/exception_handling.py
@@ -17,3 +17,29 @@ except ValueError:
 
 else:
     print("Nice ")
+
+
+# finally 
+    #  sucess or not ==> conncetion is close finally 
+    # file clean up /conncetion close 
+    # n the end.
+    # 
+    # database kholeu hamle try and except block ma code j sukai operation hos finally ma chai conncetion close hunxa . jasari pani ending ma define hunxa 
+    # 
+    # You can use finally to make sure files or resources are closed or released regardless of whether an exception occurs, 
+
+
+
+
+# -------------------------------------------------important topic of the finally block of exception handling --------------------------------------
+
+    # The finally block is useful for cleanup code that must be executed under all circumstances. 
+    # For example, if you open a file in a try block, you would close it in the finally block to 
+    # ensure the file gets closed whether the operation was successful or an exception occurred.
+
+
+# ///////////////////////////code cleanup ko laghi finally use garxau hamle ////////////////////////////////////////////////
+
+
+
+
